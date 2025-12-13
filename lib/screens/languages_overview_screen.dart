ac3f150af1027a866f3347e4b71f5cbc639da70e
d-a-vi-d <daverafi30@gmail.com>
2025-12-13 11:53:08 +0700

    languages overview screen bissi weiter gemacht, hat jetzt schon die sprachen als boxen

--- lib/screens/languages_overview_screen.dart
+++ lib/screens/languages_overview_screen.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:voc_trainer/services/word_service.dart';
 
 class LanguagesOverviewScreen extends StatelessWidget {
   //final
   const LanguagesOverviewScreen();
 
+  Widget _buildLanguagetile(int index) => Container(
+    margin: const EdgeInsets.all(5),
+    decoration: BoxDecoration(
+      color: Colors.green,
+      borderRadius: BorderRadius.circular(15),
+      border: Border.all(color: Colors.black, width: 3),
+    ),
+    alignment: Alignment.center,
+    padding: const EdgeInsets.all(10),
+    child: Text(WordService.languages[index], style: TextStyle(fontSize: 20)),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return ReorderableListView(
-      onReorder: (hallo, hall) {},
-      children: [
-        Text(key: ValueKey(1), "hallo"),
-        Text(key: ValueKey(2), "hi"),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        actionsPadding: EdgeInsets.only(right: 8),
+        title: Text("Language Overview"),
+        actions: [],
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+        child: SizedBox(
+          height: 90,
+          child: Column(
+            children: <Widget>[
+              for (
+                int index = 0;
+                index < WordService.languages.length;
+                index += 1
+              )
+                Container(child: _buildLanguagetile(index)),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
